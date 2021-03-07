2a24f4f2a048c418eb2d4466e333d4a0cb26e7ff
XanderJC <alexjameschan@gmail.com>
2021-03-07 15:40:45 +0000

    seperate dataset from domain

--- medkit/bases/__init__.py
+++ medkit/bases/__init__.py
@@ -1,5 +1,6 @@
 from .base_reward import BaseReward
-from .base_domain import BaseDomain,standard_dataset
+from .base_domain import BaseDomain
+from .base_dataset import standard_dataset
 from .base_env import BaseEnv
 from .base_initialiser import BaseInit
 from .base_model import BaseModel
